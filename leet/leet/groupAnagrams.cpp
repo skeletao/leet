1c6feaea604582e36e5fe02e5ff9ea1f8129fd08
Xiaotao Zhou <xiaotao.zhou@wdc.com>
2018-06-15 14:16:36 +0800

    Add groupAnagrams

--- leet/leet/groupAnagrams.cpp
+++ leet/leet/groupAnagrams.cpp
@@ -5,39 +5,52 @@
 * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
 */
 
-char strCmp(char *s1, char *s2)
+
+int calStrHash(char *str)
 {
+	int i = 0, j = 0, n = strlen(str);
 	int ff[26] = { 0 };
-
-	while (*s1)
+	int hash = 0;
+	
+	for (i = 0; i < n; i++)
 	{
-		ff[*s1-'a'] = 1;
-		s1++;
+		ff[*(str + i) - 'a']++;
 	}
 
-	while (*s2)
+	for (i = 0; i < 26; i++)
 	{
-		if (!ff[*s2-'a'])
+		for (j = 0; j < ff[i]; j++)
 		{
-			return false;
+			hash = (hash << 7) + (hash << 1) + i + 'a';
 		}
-		s2++;
 	}
-	return true;
+	return hash;
 }
 
-char*** groupAnagrams(char (*strs)[4], int strsSize, int** columnSizes, int* returnSize) {
-	int i = 0, j = 0, t = 0;
-	char ***res = NULL;
 
-	int *f = (int *)malloc(strsSize);
-	char *s1, *s2;
+char*** groupAnagrams(char (*strs)[4], int strsSize, int** columnSizes, int* returnSize) {
+	int i = 0, j = 0;
+	char ***res = 0;
+	*columnSizes = 0;
+	*returnSize = 0;
+
+	char*** result;
+	result = (char***)malloc(sizeof(char**));
+	result[0] = (char**)malloc(sizeof(char*));
+	result[0][0] = strs[0];
+
+	*columnSizes = (int*)malloc(sizeof(int));
+	(*columnSizes)[0] = 1;
 
 	*returnSize = 0;
+
+	int *f = (int *)malloc(strsSize * sizeof(int));
+	int *hashList = (int *)malloc(strsSize * sizeof(int));
 
 	for (i = 0; i < strsSize; i++)
 	{
 		f[i] = 0;
+		hashList[i] = calStrHash(strs[i]);
 	}
 
 	for (i = 0; i < strsSize; i++)
@@ -46,25 +59,20 @@ char*** groupAnagrams(char (*strs)[4], int strsSize, int** columnSizes, int* ret
 		{
 			continue;
 		}
-		s1 = strs[i];
 		f[i] = 1;
-		*returnSize += 1;
-		t = 1;
 
-		if (*returnSize == 1)
+		if (i > 0)
 		{
-			*columnSizes = (int *)malloc(sizeof(int));
-			res = (char ***)malloc(sizeof(char **));
-		}
-		else
-		{
-			*columnSizes = (int *)realloc(*columnSizes, sizeof(int)*(*returnSize));
-			res = (char ***)realloc(res, sizeof(char **)*(*returnSize));
+			*returnSize += 1;
+			result = (char ***)realloc(result, (*returnSize + 1) * sizeof(char**));
+			result[*returnSize] = (char**)malloc(sizeof(char*));
+			*columnSizes = (int *)realloc(*columnSizes, (*returnSize + 1) * sizeof(int));
+			(*columnSizes)[*returnSize] = 0;
 
+			(*columnSizes)[*returnSize] += 1;
+			result[*returnSize] = (char **)realloc(result[*returnSize], ((*columnSizes)[*returnSize] + 1) * sizeof(char*));
+			result[*returnSize][(*columnSizes)[*returnSize] - 1] = strs[i];
 		}
-
-		res[*returnSize - 1] = (char **)malloc(sizeof(char *));
-		res[*returnSize - 1][0] = s1;
 
 		for (j = i + 1; j < strsSize; j++)
 		{
@@ -72,18 +80,17 @@ char*** groupAnagrams(char (*strs)[4], int strsSize, int** columnSizes, int* ret
 			{
 				continue;
 			}
-			s2 = strs[j];
 
-			if (strCmp(s1, s2))
+			if (hashList[i] == hashList[j])
 			{
 				f[j] = 1;
-				t++;
-				res[*returnSize - 1] = (char **)realloc(res[*returnSize - 1], sizeof(char *)*t);
-				res[*returnSize - 1][t - 1] = s2;
+				(*columnSizes)[*returnSize] += 1;
+				result[*returnSize] = (char **)realloc(result[*returnSize], ((*columnSizes)[*returnSize] + 1) * sizeof(char*));
+				result[*returnSize][(*columnSizes)[*returnSize] - 1] = strs[j];
 			}
 		}
-		columnSizes[0][*returnSize - 1] = t;
 	}
-	return res;
-}
 
+	*returnSize += 1;
+	return result;
+}
--- leet/leet/main.cpp
+++ leet/leet/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char **argv)
 {
 	int nums1[] = { 1, 2, 3 };
 	int nums2[][4] = { { 0, 1, 2, 3 },{ 4, 5, 6, 7 },{ 8, 9, 10, 11 },{ 12, 13, 14, 15 } };
-	char strs1[] = "aa";
-	char strs2[][4] = { "cab","tin","pew","duh","may","ill","buy","bar","max","doc" };
+	char strs1[] = "dfgalsagewqtoudgxbms";
+	char strs2[][4] = { "eat","tea","tan","ate","nat","bat" };
 	int *returnSize = (int *)malloc(sizeof(int));
 	int **columnSizes = (int **)malloc(sizeof(int *));
 	groupAnagrams(strs2, sizeof(strs2)/sizeof(strs2[0]), columnSizes, returnSize);
